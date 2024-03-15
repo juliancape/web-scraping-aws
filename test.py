9c5e55fe574e875db086ec9f8f60a66c4cc67366
juliancape <juliancape360@gmail.com>
2024-03-15 09:53:05 -0500

    Add more test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
+from transformar_html import transform_html
 
 def test_real_request_status_code():
     url = "https://casas.mitula.com.co/searchRE/orden-0/op-1/q-bogot%C3%A1/pag-1?req_sgmt=REVTS1RPUDtVU0VSX1NFQVJDSDtTRVJQOw=="
@@ -8,3 +9,6 @@ def test_real_request_status_code():
     
     # Verificar que el código de estado sea 200
     assert response.status_code == 200
+
+def test_transform():
+    transform_html(test = True)
--- transformar_html.py
+++ transformar_html.py
@@ -4,68 +4,83 @@ import pandas as pd
 from datetime import datetime
 
 
-def transform_html():
+def transform_html(test=False):
     """
     Transformar el html previamente descargardo
     y generar un csv que se guarde en un bucket S3
     """
-    print('probandooo')
-    s3 = boto3.client('s3')
-    bucket_name = 'zappa-bucket-julian-descargar'
-    folder_prefix = 'casas_raw/'
+    if test is True:
+        with open("html_test.html", "r", encoding="utf-8") as file:
+            html_content = file.read()
+        # Parsea el contenido HTML
+        soup = BeautifulSoup(html_content, "html.parser")
+        card = soup.find(
+            'div', class_='listing listing-card')
+        data = {
+            'href': card.get('data-href', ''),
+            'price': card.get('data-price', ''),
+            'currency': card.get('data-currency', ''),
+            'operation_type': card.get('data-operation-type', ''),
+            'rooms': card.get('data-rooms', ''),
+            'location': card.get('data-location', ''),
+            'floor_area': card.get('data-floorarea', ''),
+            'viewed': card.get('data-viewed', '')
+        }
+        print(data)
+    else:
+        s3 = boto3.client('s3')
+        bucket_name = 'zappa-bucket-julian-descargar'
+        folder_prefix = 'casas_raw/'
 
-    # Obtener enlaces de archivos HTML del bucket
-    print('Hola')
-    response = s3.list_objects(Bucket=bucket_name, Prefix=folder_prefix)
-    print('chao')
+        # Obtener enlaces de archivos HTML del bucket
+        response = s3.list_objects(Bucket=bucket_name, Prefix=folder_prefix)
+        for obj in response['Contents']:
+            key = obj['Key']
 
-    for obj in response['Contents']:
-        key = obj['Key']
+            if key.endswith('.html'):
+                response = s3.get_object(Bucket=bucket_name, Key=key)
+                html_content = response['Body'].read()
 
-        if key.endswith('.html'):
-            response = s3.get_object(Bucket=bucket_name, Key=key)
-            html_content = response['Body'].read()
+                soup = BeautifulSoup(html_content, 'html.parser')
+                # Buscar todos los elementos div con la clase 'listing
+                # listing-card'
+                listings_cards = soup.find_all(
+                    'div', class_='listing listing-card')
 
-            soup = BeautifulSoup(html_content, 'html.parser')
-            # Buscar todos los elementos div con la clase 'listing
-            # listing-card'
-            listings_cards = soup.find_all(
-                'div', class_='listing listing-card')
+                # Crear una lista para almacenar los datos
+                data_list = []
 
-            # Crear una lista para almacenar los datos
-            data_list = []
+                # Iterar sobre cada elemento 'listing-card'
+                # deseados
+                for card in listings_cards:
+                    data = {
+                        'href': card.get('data-href', ''),
+                        'price': card.get('data-price', ''),
+                        'currency': card.get('data-currency', ''),
+                        'operation_type': card.get('data-operation-type', ''),
+                        'rooms': card.get('data-rooms', ''),
+                        'location': card.get('data-location', ''),
+                        'floor_area': card.get('data-floorarea', ''),
+                        'viewed': card.get('data-viewed', '')
+                    }
+                    data_list.append(data)
 
-            # Iterar sobre cada elemento 'listing-card' para obtener los datos
-            # deseados
-            for card in listings_cards:
-                data = {
-                    'href': card.get('data-href', ''),
-                    'price': card.get('data-price', ''),
-                    'currency': card.get('data-currency', ''),
-                    'operation_type': card.get('data-operation-type', ''),
-                    'rooms': card.get('data-rooms', ''),
-                    'location': card.get('data-location', ''),
-                    'floor_area': card.get('data-floorarea', ''),
-                    'viewed': card.get('data-viewed', '')
-                }
-                data_list.append(data)
+                # Crear un DataFrame de Pandas
+                df = pd.DataFrame(data_list)
 
-            # Crear un DataFrame de Pandas
-            df = pd.DataFrame(data_list)
+                # Mostrar el DataFrame
+                print(df)
+                # Convertir DataFrame a formato CSV
+                df = df.to_csv(index=False, sep=';')
+                # Obtener la fecha actual
+                nombre = str(datetime.today().strftime('%Y-%m-%d'))
 
-            # Mostrar el DataFrame
-            print(df)
-            # Convertir DataFrame a formato CSV
-            df = df.to_csv(index=False, sep=';')
-            # Obtener la fecha actual
-            nombre = str(datetime.today().strftime('%Y-%m-%d'))
-
-            # Subir archivo CSV a S3 con estructura de carpetas basada en la
-            # fecha
-            print(df)
-            s3.put_object(Body=df.replace('"', ''),
-                          Bucket='zappa-bucket-julian-transformar',
-                          Key=str('casas_final/casas/' +
-                                  'year=' + nombre[:4] + '/month='
-                                  + nombre[5:7] +
-                                  '/day=' + nombre[8:] + f'/{nombre}.csv'))
+                # Subir archivo CSV a S3 con estructura de
+                # carpetas basada en la fecha
+                print(df)
+                s3.put_object(Body=df.replace('"', ''),
+                              Bucket='zappa-bucket-julian-transformar',
+                              Key=str('casas_final/casas/' +
+                                      'year=' + nombre[:4] + '/month='
+                                      + nombre[5:7] +
+                                      '/day=' + nombre[8:] + f'/{nombre}.csv'))
